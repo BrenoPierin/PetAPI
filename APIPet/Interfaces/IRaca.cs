8f9b076767f548a29dd6cc443630e791b6ef842d
unknown <brenomax02@gmail.com>
2020-09-03 14:24:38 -0300

    Projeto iniciado

---
+++ APIPet/Interfaces/IRaca.cs
@@ -0,0 +1,17 @@
+﻿using APIPet.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPet.Interfaces
+{
+    interface IRaca
+    {
+        List<Raca> ListarTodos();
+        Raca BuscarPorID(int id);
+        Raca Cadastrar(Raca pet);
+        Raca Alterar(int id, Raca pet);
+        void Excluir(int id);
+    }
+}
---
+++ APIPet/Repositories/TipoPetRepository.cs
@@ -0,0 +1,37 @@
+﻿using APIPet.Domains;
+using APIPet.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPet.Repositories
+{
+    public class TipoPetRepository : ITipoPet
+    {
+        public TipoPet Alterar(int id, TipoPet pet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoPet BuscarPorID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoPet Cadastrar(TipoPet pet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TipoPet> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
